dcff8ba1e71c1154b38daf03cf55d4755fc9daa0
Omar <ocontreras.sf@gmail.com>
2018-10-06 17:57:14 +0000

    Add script to export API data to JSON file

---
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Script to gather data from an API
+import json
+import requests
+import sys
+
+
+def get_users(base_url):
+    """Gets users list
+       Args:
+           base_url (str): base url for API
+       Returns: list of users (dicts)
+
+    """
+    response = requests.get("{}users/".format(base_url))
+    return response.json()
+
+
+def get_todo_list(base_url, user_id):
+    """Gets todo list for specific user
+       Args:
+           base_url (str): base url for API
+           user_id (str): user id number
+       Returns: list of todo items (dicts)
+    """
+    response = requests.get(
+        "{}users/{}/todos".format(base_url, user_id))
+    return response.json()
+
+
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    users_dict = {}
+    users_json = get_users(base_url)
+    for user in users_json:
+        user_id = user['id']
+        username = user['username']
+        users_dict[user_id] = []
+        todo_list = get_todo_list(base_url, user_id)
+        for todo in todo_list:
+            todo_dict = {}
+            todo_dict['username'] = username
+            todo_dict['task'] = todo['title']
+            todo_dict['completed'] = todo['completed']
+            users_dict[user_id].append(todo_dict)
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_dict, f)
--- 0x01-warm_up/README.md
+++ 0x01-warm_up/README.md
@@ -1,2 +1,3 @@
 # Warm Up
 * 0-binary.c: binary search
+* 1-dictionary_of_list_of_dictionaries.py: export data in the JSON format
