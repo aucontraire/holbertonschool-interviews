145a887153cce6471014d564e3ecc73606712048
Omar <ocontreras.sf@gmail.com>
2018-10-06 18:42:53 +0000

    Add script that checks if binary tree is full

---
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if binary tree is full
+ * @tree: pointer to a tree struct
+ *
+ * Return: 1 if full, 0 if not
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+
+	if (tree->right && tree->left)
+	{
+		return (binary_tree_is_full(tree->right) &&
+			binary_tree_is_full(tree->left));
+	}
+
+	return (0);
+}
--- 0x01-warm_up/README.md
+++ 0x01-warm_up/README.md
@@ -1,3 +1,4 @@
 # Warm Up
 * 0-binary.c: binary search
 * 1-dictionary_of_list_of_dictionaries.py: export data in the JSON format
+* 2-binary_tree_is_full.c: checks if binary tree is full
